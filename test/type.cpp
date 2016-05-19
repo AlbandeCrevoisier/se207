0cd6ab41c2f527cd51dd1fc6768c86287395e0b7
ACH <albandecrevoisier@gmail.com>
2016-05-19 23:50:49 +0200

    Test sc_logic.

--- test/type.cpp
+++ test/type.cpp
@@ -1,21 +1,16 @@
 /* Testing type logic */
 #include <systemc.h>
 #include <iostream>
+using namespace std;
 
 int
 sc_main(int argc, char *argv[])
 {
-    sc_logic u(1);
-    cout << u << endl;
-
-    u = 0;
-    cout << u << endl;
-    
-    u = 'z';
-    cout << u << endl;
-
-    u = 'x';
-    cout << u << endl;
+    sc_logic f(0), t(1), z('z'), x('x');
+    sc_logic i(3);
+    cout << f << t << z << x << i << endl;
+    cout << ~x << (f ^ t) << (z & x) << endl;
+    cout << (f | true) << (true | x) << endl;
 
     return 0;
 }
