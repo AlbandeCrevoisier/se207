ab5930979cf8d4f9ea1c7d58115fc6d1d4680f65
ACH <albandecrevoisier@gmail.com>
2016-06-10 12:12:29 +0200

    Trace: WIP

--- test/trace.cpp
+++ test/trace.cpp
@@ -2,27 +2,30 @@
 #include <systemc.h>
 
 int
-sc_main (int argc, char * argv[])
+sc_main (int argc, char *argv[])
 {
+	uint8_t n = atoi(argv[1]);
+	uint8_t idx = 0;
+
 	// Un pointeur sur l'objet qui permet de gérer les traces
 	sc_trace_file *trace_f;
 
 	/* Cette fonction crée l'objet
-	 * L'argument est le nom du fichier qui sera créer.
+	 * L'argument est le nom du fichier qui sera créé.
 	 * L'extension .vcd est ajoutée automatiquement
 	 */
 	trace_f = sc_create_vcd_trace_file ("my_simu_trace");
 	// On peut aussi préciser l'unité de temps dans le fichier vcd
 	trace_f->set_time_unit(1,SC_NS);
 
-	bool t;
-
+	
 	/* Ajoute la variable t aux éléments à tracer
 	 * Les arguments de la fonction sont:
 	 *  - le pointeur vers le fichier de trace
 	 *  - la variable/objet à tracer
 	 *  - le nom dans dans le fichier généré
 	 */
+	bool t;
 	sc_trace(trace_f, t, "t");
 
 	int i;
