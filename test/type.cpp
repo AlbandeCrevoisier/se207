6294950dab613c6e1a87a6432a8e1631da71c46e
ACH <albandecrevoisier@gmail.com>
2016-06-10 11:34:15 +0200

    Add sc_lv and sc_bv to type test.

--- test/type.cpp
+++ test/type.cpp
@@ -8,12 +8,24 @@ sc_main(int argc, char *argv[])
 	bool bf =  false, bt = true;
 	sc_logic f(0), t(1), z('z'), x('x');
 	sc_logic i(3);
+	sc_lv<8> lvb = "01ZX01ZX";
+	sc_lv<8> lvc = 42;
+	sc_lv<8> lvd = false;
+	sc_bv<8> bvb = 9;
+	sc_bv<8> bvc = true;
+	sc_lv<4> lve = bvb(6, 3);
+	sc_bv<4> bvd = lvb(6, 3);
 
 	cout << f << t << z << x << i << endl;
 	cout << ~x << (f ^ t) << (z & x) << endl;
 	cout << (f | false) << (x | false) << (z | false) << endl;
-
 	cout << (bf | t) << (bt ^ z) << (bf & i) << endl;
+
+	cout << lvb << endl << lvc << endl << lvd << endl;
+	cout << bvb << endl << bvc << endl;
+	cout << (lvb ^ lvc) << endl << (lvc & lvd) << endl;
+	cout << (lvb ^ bvb) << endl << (lvc | bvc) << endl;
+	cout << lve << endl << bvd << endl;
 
 	return 0;
 }
